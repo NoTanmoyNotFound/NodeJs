fc3f3039af3fab706e7bd93a7ef7321588dcf47e
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-03-26 10:53:21 +0530

    adding file2.txt

--- fsmodule.js
+++ fsmodule.js
@@ -4,7 +4,12 @@ const fs = require('fs');
 //     console.log(err,data);
 // } )
 
-const bruh = fs.readFileSync('file.txt')
-console.log(bruh.toString());
+
+// const bruh = fs.readFileSync('file.txt')
+// console.log(bruh.toString());
+
+fs.writeFile('file2.txt', "This is file 2 content",()=>{
+    console.log("file changed");
+})
 
 console.log("Finishing reading file");
